dfb0522918f6975aba2ded5126f3e29105700af2
olivebishop <olivehendrilgen1@gmail.com>
2024-12-23 11:53:42 +0300

    feat: added main file

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "timetrack/cmd"
+)
+
+func main() {
+    if err := cmd.Execute(); err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+}
